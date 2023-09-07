dd7d364dba46eb79371bc467199ebba418da1bcd
Aman Verma <avman1998@gmail.com>
2023-09-07 14:39:43 +0530

    customeSortAlgo

--- customSortAlgorithm.js
+++ customSortAlgorithm.js
@@ -1,6 +1,6 @@
 const arr = [56, 2, 3, 4, 5, 6, 7];
 const compare = (a, b) => b - a;
-console.log(arr.sort(compare));
+// console.log(arr.sort(compare));
 
 const arr2 = [
   {
@@ -19,4 +19,16 @@ const arr2 = [
 const compare2 = (a, b) => {
   return a.items.length - b.items.length;
 };
-console.log(arr2.sort(compare2));
+// console.log(arr2.sort(compare2));
+
+const arr3 = [1, 2, 3, 4, "ba", "lo", "red", 80, 56];
+const isNumeric = (number) => !isNaN(number);
+
+function compare3(a, b) {
+  if (isNumeric(a) && isNumeric(b)) return a - b;
+  if (isNumeric(a) && !isNumeric(b)) return -1;
+  if (!isNumeric(a) && isNumeric(b)) return 1;
+  return a > b ? 1 : -1;
+}
+
+console.log(arr3.sort(compare3));
